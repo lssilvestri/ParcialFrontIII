e8a6e2c7ad2903babfd3273205fc6dd17d8cfa60
lssilvestri <lucas.silvestri1a@gmail.com>
2024-11-08 20:39:56 -0300

    Form y Card con estilos terminado

--- Parcial/src/App.jsx
+++ Parcial/src/App.jsx
@@ -1,15 +1,53 @@
-import './App.css'
-import Card from './components/Card'
+import { useState } from 'react';
+import './styles/App.css';
+import Card from './components/Card';
 
 function App() {
+  const [nombre, setNombre] = useState("");
+  const [direccion, setDireccion] = useState("");
+  const [err, setError] = useState(false);
+  const [showCard, setShowCard] = useState(false);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (nombre.trim().length >= 3 && direccion.trim().length >= 6) {
+      setError(false);
+      setShowCard(true); 
+    } else {
+      setError(true);
+      setShowCard(false);
+    }
+  };
 
   return (
-    <div className='App'>
-      <h1>Carga de estudiantes</h1>
-      <form></form>
-      <Card/>
+    <div className="App">
+      <h1>Elige la última canción que escuchaste</h1>
+      <form onSubmit={submitHandler}>
+        <div>
+          <input
+            type="text"
+            placeholder="Ingresa tu nombre"
+            value={nombre}
+            onChange={(e) => setNombre(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Ingresa la canción"
+            value={direccion}
+            onChange={(e) => setDireccion(e.target.value)}
+          />
+        </div>
+        <button type="submit">ENVIAR</button>
+        {err && (
+          <h3 style={{ color: "red" }}>
+            Por favor, chequea que la información sea correcta
+          </h3>
+        )}
+      </form>
+      {showCard && <Card nombre={nombre} direccion={direccion} />}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
+
--- Parcial/src/components/Card.jsx
+++ Parcial/src/components/Card.jsx
@@ -1,7 +1,12 @@
-const Card = () => {
+import CardStyles from "../styles/Card.module.css";
+function Card({ nombre, direccion }) {
   return (
-    <div>Hola, soy una tarjeta</div>
-  )
+    <div className={CardStyles.card}>
+      <h2>Información Ingresada</h2>
+      <p><strong>Nombre:</strong> {nombre}</p>
+      <p><strong>Canción:</strong> {direccion}</p>
+    </div>
+  );
 }
 
-export default Card
+export default Card;
